3d3de4782882ecc239b266e0b11761a498924749
tausif71 <91406350+tausif71@users.noreply.github.com>
2022-05-20 12:05:04 +0530

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,21 +2,21 @@
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
 
-importScripts('https://www.gstatic.com/firebasejs/7.23.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.23.0/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js");
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
 firebase.initializeApp({
     // REPLACE BY YOUR FIREBASE CONFIG HERE
-    apiKey: "AIzaSyAvy3mKilnCvK4Eq4HoORmjjtgcGZFekyY",
-    authDomain: "attendanceapp-96453.firebaseapp.com",
-    projectId: "attendanceapp-96453",
-    storageBucket: "attendanceapp-96453.appspot.com",
-    messagingSenderId: "742454421899",
-    appId: "1:742454421899:web:3ea83cf38023555fb5278e",
-    measurementId: "G-E0GKCKY7XY"
+    apiKey: "AIzaSyAMF__fZfJr_n4TQN3fqH_3FMwX6cDkql0",
+    authDomain: "notificationwebportal.firebaseapp.com",
+    projectId: "notificationwebportal",
+    storageBucket: "notificationwebportal.appspot.com",
+    messagingSenderId: "540556220617",
+    appId: "1:540556220617:web:0647a71b177ba2a726d518",
+    measurementId: "G-VFQLHPR6MB"
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
